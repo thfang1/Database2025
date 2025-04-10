075cf15fa860cc92cf02747a015cc7d29b94fd9c
CISC181 <bgibbons@udel.edu>
2025-04-10 12:52:27 -0400

    Update Program.cs

--- WebRestAPI/Program.cs
+++ WebRestAPI/Program.cs
@@ -27,7 +27,9 @@ var mapperConfig = new MapperConfiguration(mc =>
 IMapper mapper = mapperConfig.CreateMapper();
 builder.Services.AddSingleton(mapper);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(x =>
+   x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
